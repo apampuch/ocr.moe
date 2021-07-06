8c9fd80600dfe46d3a0356da800e48896de62633
Andrew <apampuch@protonmail.com>
2021-07-05 23:05:50 -0700

    Fixes layout and adds second layer.

--- ocr-canvas.js
+++ ocr-canvas.js
@@ -1,17 +1,44 @@
-// get size of image area select elementFromPoint
-var canvasWidth = document.getElementById("image-area-select").width
-var canvasHeight = document.getElementById("image-area-select").height
+class Point {
+    constructor (x,y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 // draw a line on the canvas
-var c = document.getElementById("image-area-select");
-var ctx = c.getContext("2d");
+var imageCanvas = document.getElementById("image-layer");
+var imageCtx = imageCanvas.getContext("2d");
 
-ctx.moveTo(0,0);
-ctx.lineTo(200,200);
-ctx.stroke();
+// draw a line on the canvas
+var cropCanvas = document.getElementById("crop-layer");
+var cropCtx = cropCanvas.getContext("2d");
+
+// get size of image area select elementFromPoint
+// both canvases should be EXACTLY the same size
+var canvasWidth = imageCanvas.width;
+var canvasHeight = imageCanvas.height;
+
+imageCtx.strokeStyle = "#0000FF";
+imageCtx.moveTo(0,0);
+imageCtx.lineTo(canvasWidth,canvasHeight);
+imageCtx.stroke();
+
+cropCtx.strokeStyle = "#FF0000";
+cropCtx.moveTo(canvasWidth,0);
+cropCtx.lineTo(0,canvasHeight);
+cropCtx.stroke();
+
+cropCanvas.onclick = cropClick
+
+// setup points for crop
+var topLeft     = new Point(0,0)
+var bottomLeft  = new Point(0,canvasHeight)
+var topRight    = new Point(canvasWidth,0)
+var bottomRight = new Point(canvasWidth,canvasHeight)
+
 
 function loadImage() {
-    ctx.clearRect(0, 0, c.width, c.height);
+    imageCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
     
     // get the image from the upload
     var file = document.getElementById("file-input").files[0]
@@ -27,7 +54,7 @@ function loadImage() {
             // center the image
             var pad = (canvasHeight - gappedEdge) / 2
             
-            ctx.drawImage(img, 0, pad, canvasWidth, gappedEdge);
+            imageCtx.drawImage(img, 0, pad, canvasWidth, gappedEdge);
         } else {
             var divisor = img.height / canvasHeight;
             var gappedEdge = img.width / divisor;
@@ -35,10 +62,13 @@ function loadImage() {
             // center the image
             var pad = (canvasWidth - gappedEdge) / 2
             
-            ctx.drawImage(img, pad, 0, gappedEdge, canvasHeight);
+            imageCtx.drawImage(img, pad, 0, gappedEdge, canvasHeight);
         }        
     }
     
     img.src = URL.createObjectURL(file)
 }
 
+function cropClick(event) {
+    alert("Crop clicked.");
+}
