3285361c2bc95bd9879a2edaf83d7b6948a35a01
Andrew <apampuch@protonmail.com>
2021-07-15 10:28:06 -0700

    Adds interface with Tesseract.

--- upload.php
+++ upload.php
@@ -4,8 +4,19 @@ if ( 0 < $_FILES['image']['error'] ) {
     echo 'Error: ' . $_FILES['image']['error'] . '<br>';
 }
 else {
-    move_uploaded_file($_FILES['image']['tmp_name'], 'images/' . $_FILES['image']['name']);
+    // get a hopefully unique name for the image
+    $temp_name = str_replace("/tmp/php", "", $_FILES['image']['tmp_name']);
+    
+    // TODO when we have a database, add mime type and timestamps and stuff
+    
+    // move the file to the images folder
+    move_uploaded_file($_FILES['image']['tmp_name'], 'images/' . $temp_name);
+    $command = "tesseract images/" . $temp_name . " -";
+    $result = null;
+    
+    exec($command, $result);
 
+    print_r($result);
 }
 ?>
 
