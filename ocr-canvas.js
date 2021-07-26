adb1bbe9b187f176217bbe7cc7db71f13fdcc9bc
Andrew <apampuch@protonmail.com>
2021-07-25 23:21:03 -0700

    Crop marker now drags corners and edges.

--- ocr-canvas.js
+++ ocr-canvas.js
@@ -28,7 +28,9 @@ cropCtx.moveTo(canvasWidth,0);
 cropCtx.strokeRect(0,0, canvasWidth, canvasHeight);
 cropCtx.stroke();
 
-var dragging = undefined;
+var dragging = undefined; // the point or edge (array of two points) we're dragging
+var draggingDir = undefined; // used only for dragging edges
+var draggingType = undefined; // dragging a corner or an edge
 
 cropCanvas.addEventListener('mousedown', cropMouseDown);
 cropCanvas.addEventListener('mousemove', cropMouseMove);
@@ -79,14 +81,15 @@ function loadImage() {
 }
 
 function cropMouseDown(event) {
+    
     // check if we're clicking near a point
-    const POINT_RADIUS = 20;
+    const POINT_RADIUS = 20;    
     
     var x = event.clientX - cropCanvas.getBoundingClientRect().left;
     var y = event.clientY - cropCanvas.getBoundingClientRect().top;
     
-    
     for (let i=0; i<points.length; i++) {
+    
         // get distance
         var d = distance(x, y, points[i].x, points[i].y);
         
@@ -96,8 +99,42 @@ function cropMouseDown(event) {
         // grab it
         if (d <= POINT_RADIUS) {
             dragging = i;
+            draggingType = "corner";
+            return;
         }
     }
+    
+    // if we're not clicking near a point, check if we're near an edge
+    // left
+    if (edge_check(x, y, topLeft.y, bottomLeft.y, topLeft.x, "x", Math.abs(x - topLeft.x))) {
+        dragging = [topLeft, bottomLeft];
+        draggingDir = "x";
+        draggingType = "edge";
+    }
+    // right
+    else if (edge_check(x, y, topRight.y, bottomRight.y, bottomRight.x, "x", Math.abs(x - bottomRight.x))) {
+        dragging = [topRight, bottomRight];
+        draggingDir = "x";
+        draggingType = "edge";
+    }
+    // top
+    else if (edge_check(x, y, topLeft.x, topRight.x, topLeft.y, "y", Math.abs(y - topLeft.y))) {
+        dragging = [topLeft, topRight];
+        draggingDir = "y";
+        draggingType = "edge";
+    }
+    // bottom
+    else if (edge_check(x, y, bottomLeft.x, bottomRight.x, bottomRight.y, "y", Math.abs(y - bottomRight.y))) {
+        dragging = [bottomLeft, bottomRight];
+        draggingDir = "y";
+        draggingType = "edge";
+    } 
+    // otherwise do nothing
+    else {
+        return;
+    }
+    
+
 }
 
 function cropMouseMove(event) {
@@ -106,16 +143,33 @@ function cropMouseMove(event) {
         var x = event.clientX - cropCanvas.getBoundingClientRect().left;
         var y = event.clientY - cropCanvas.getBoundingClientRect().top;
         
-        // get the point we're dragging
-        drag_point = points[dragging];
-        
-        // update point we're dragging
-        drag_point.x = x;
-        drag_point.y = y;
-        
-        // update adjacent points
-        adjacentPoints[dragging][0].x = x;
-        adjacentPoints[dragging][1].y = y;
+        if (draggingType == "corner") {
+            // get the point we're dragging
+            drag_point = points[dragging];
+            
+            // update point we're dragging
+            drag_point.x = x;
+            drag_point.y = y;
+            
+            // update adjacent points
+            adjacentPoints[dragging][0].x = x;
+            adjacentPoints[dragging][1].y = y;
+        }
+        else if (draggingType == "edge") {
+            if (draggingDir == "x") {
+                dragging[0].x = x;
+                dragging[1].x = x;
+            }
+            else if (draggingDir == "y") {
+                dragging[0].y = y;
+                dragging[1].y = y;
+            }
+            else {
+                throw "Dragging direction not set!";
+            }
+        } else {
+            throw "Dragging type not set!";
+        }
         
         cropCtx.clearRect(0,0,canvasWidth,canvasHeight);
         cropCtx.beginPath();
@@ -125,6 +179,22 @@ function cropMouseMove(event) {
 
 function cropMouseUp(event) {
     dragging = undefined;
+    draggingDir = undefined;
+    draggingType = undefined;
+}
+
+function edge_check(x, y, lower_limit, upper_limit, line_location, dimension, dist) {
+    const EDGE_RADIUS = 50;
+
+    if (dimension == "x") {    
+        // between the top and bottom corners
+        return (lower_limit < y && y < upper_limit) && dist <= EDGE_RADIUS;
+    } else if (dimension == "y") {
+        // between the left and right corners
+        return (lower_limit < x && x < upper_limit) && dist <= EDGE_RADIUS;
+    } else {
+        throw new Exception("Invalid dimension " + dimension + ".");
+    }
 }
 
 function distance(x1,y1,x2,y2) {
